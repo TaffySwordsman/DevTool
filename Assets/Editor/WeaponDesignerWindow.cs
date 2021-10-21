cb1a189219d4e8e1b43bf938013650a64a845ead
Xander <xanderyoussef@gmail.com>
2021-10-20 22:14:28 -0500

    Center text and display part names

--- Assets/Editor/WeaponDesignerWindow.cs
+++ Assets/Editor/WeaponDesignerWindow.cs
@@ -7,11 +7,14 @@ public class WeaponDesignerWindow : EditorWindow
 {
     Texture2D noWeapon, noAttachment;
     Texture2D baseTexture, opticsTexture, barrelTexture, magTexture;
+    Texture2D mainTex, dispTex, attachTex, weapTex;
+
     WeaponBaseData weaponBase;
     BarrelData barrel;
     OpticsData optics;
     MagazineData magazine;
-    Texture2D mainTex, dispTex, attachTex, weapTex;
+
+    string weaponName, barrelName, opticsName, magName;
 
     Rect mainSection;
     Rect displaySection;
@@ -39,19 +42,19 @@ public class WeaponDesignerWindow : EditorWindow
 
     void InitTextures()
     {
-        mainTex = new Texture2D(1,1);
+        mainTex = new Texture2D(1, 1);
         mainTex.SetPixel(0, 0, mainColor);
         mainTex.Apply();
 
-        dispTex = new Texture2D(1,1);
+        dispTex = new Texture2D(1, 1);
         dispTex.SetPixel(0, 0, displayColor);
         dispTex.Apply();
 
-        attachTex = new Texture2D(1,1);
+        attachTex = new Texture2D(1, 1);
         attachTex.SetPixel(0, 0, weaponColor);
         attachTex.Apply();
 
-        weapTex = new Texture2D(1,1);
+        weapTex = new Texture2D(1, 1);
         weapTex.SetPixel(0, 0, attachmentColor);
         weapTex.Apply();
 
@@ -61,6 +64,9 @@ public class WeaponDesignerWindow : EditorWindow
 
     void OnGUI()
     {
+        var centeredStyle = GUI.skin.GetStyle("Label");
+        centeredStyle.alignment = TextAnchor.UpperCenter;
+
         DrawLayouts();
         DrawHeader();
     }
@@ -83,9 +89,9 @@ public class WeaponDesignerWindow : EditorWindow
         attachmentSection.height = displaySection.height * 0.33f;
 
         weaponSection.x = displaySection.x;
-        weaponSection.y = displaySection.y + displaySection.height * 0.33f;
+        weaponSection.y = displaySection.y + displaySection.height * 0.33f + 30;
         weaponSection.width = displaySection.width;
-        weaponSection.height = displaySection.height * 0.66f;
+        weaponSection.height = displaySection.height * 0.66f - 30;
 
         barrelSection.x = attachmentSection.x;
         barrelSection.y = attachmentSection.y;
@@ -111,44 +117,69 @@ public class WeaponDesignerWindow : EditorWindow
         GUILayout.BeginArea(mainSection);
         EditorGUILayout.BeginVertical();
         EditorGUILayout.Space();
-        
+
         weaponBase = (WeaponBaseData)EditorGUILayout.ObjectField("Weapon Base", weaponBase, typeof(WeaponBaseData), false);
         // If "None" selected
-        if(weaponBase == null)
+        if (weaponBase == null)
+        {
             baseTexture = noWeapon;
+            weaponName = "Empty";
+        }
         else
+        {
             baseTexture = weaponBase.image;
+            weaponName = weaponBase.partName;
+        }
 
         barrel = (BarrelData)EditorGUILayout.ObjectField("Barrel", barrel, typeof(BarrelData), false);
         // If "None" selected
-        if(barrel == null)
+        if (barrel == null)
+        {
             barrelTexture = noAttachment;
+            barrelName = "Empty";
+        }
         else
+        {
             barrelTexture = barrel.image;
+            barrelName = barrel.partName;
+        }
 
         optics = (OpticsData)EditorGUILayout.ObjectField("Optics", optics, typeof(OpticsData), false);
         // If "None" selected
-        if(optics == null)
+        if (optics == null)
+        {
             opticsTexture = noAttachment;
+            opticsName = "Empty";
+        }
         else
+        {
             opticsTexture = optics.image;
+            opticsName = optics.partName;
+        }
 
         magazine = (MagazineData)EditorGUILayout.ObjectField("Magazine", magazine, typeof(MagazineData), false);
         // If "None" selected
-        if(magazine == null)
+        if (magazine == null)
+        {
             magTexture = noAttachment;
+            magName = "Empty";
+        }
         else
+        {
             magTexture = magazine.image;
+            magName = magazine.partName;
+        }
 
         EditorGUILayout.EndVertical();
 
         GUI.DrawTexture(barrelSection, barrelTexture, ScaleMode.ScaleToFit);
-        GUI.Label(CenterTextAbove(barrelSection), "Testing 123");
+        GUI.Label(CenterTextAbove(barrelSection), barrelName);
         GUI.DrawTexture(opticsSection, opticsTexture, ScaleMode.ScaleToFit);
-        GUI.Label(CenterTextAbove(opticsSection), "Testing 123");
+        GUI.Label(CenterTextAbove(opticsSection), opticsName);
         GUI.DrawTexture(magSection, magTexture, ScaleMode.ScaleToFit);
-        GUI.Label(CenterTextAbove(magSection), "Testing 123");
+        GUI.Label(CenterTextAbove(magSection), magName);
         GUI.DrawTexture(weaponSection, baseTexture, ScaleMode.ScaleToFit);
+        GUI.Label(CenterTextAbove(weaponSection), weaponName);
         GUILayout.EndArea();
     }
 
