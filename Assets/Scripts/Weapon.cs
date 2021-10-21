e6c39006abff0de983f731d0fdc59c855cf58798
Xander <xanderyoussef@gmail.com>
2021-10-20 23:19:22 -0500

    Weapon updates based on attachments

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -8,14 +8,88 @@ public class Weapon : MonoBehaviour
     [SerializeField] BarrelData barrel;
     [SerializeField] OpticsData optics;
     [SerializeField] MagazineData magazine;
-    [ReadOnly] public string partName;
-    [ReadOnly] public MagnificationEnums magnification;
-    [ReadOnly] public float damage;
-    [ReadOnly, Range(0, 1)]
-    public float accuracy;
-    [ReadOnly, Range(0, 1)]
-    public float stability;
-    [ReadOnly] public float reloadSpeed;
-    [ReadOnly] public float fireRate;
-    [ReadOnly] public float magSize;
+    public string weaponName;
+    [ReadOnly] public MagnificationEnums magnification = MagnificationEnums.X1;
+    [ReadOnly] public float damage = 1;
+    [Range(0, 1),ReadOnly] public float accuracy = 1;
+    [Range(0, 1), ReadOnly] public float stability = 1;
+    [ReadOnly] public float reloadSpeed = 3;
+    [ReadOnly] public float fireRate = 10;
+    [ReadOnly] public float magSize = 10;
+
+    public Weapon()
+    {
+        this.weaponBase = null;
+        this.barrel = null;
+        this.optics = null;
+        this.magazine = null;
+    }
+
+    public Weapon(WeaponBaseData weaponBase = null, BarrelData barrel = null, OpticsData optics = null, MagazineData magazine = null)
+    {
+        this.weaponBase = weaponBase;
+        this.barrel = barrel;
+        this.optics = optics;
+        this.magazine = magazine;
+
+        if (weaponBase != null)
+        {
+            magnification = weaponBase.magnification;
+            damage = weaponBase.damage;
+            accuracy = weaponBase.accuracy;
+            stability = weaponBase.stability;
+            reloadSpeed = weaponBase.reloadSpeed;
+            fireRate = weaponBase.fireRate;
+            magSize = weaponBase.magSize;
+        }
+
+        if (barrel != null)
+        {
+            accuracy = Mathf.Clamp(accuracy + barrel.accuracyModifier, 0, 1);
+            stability = Mathf.Clamp(stability + barrel.stabilityModifier, 0, 1);
+        }
+
+        if (optics != null)
+        {
+            magnification = optics.magnification;
+        }
+
+        if (magazine != null)
+        {
+            magSize = magazine.magSize;
+            reloadSpeed = magazine.reloadSpeed;
+        }
+    }
+
+    void OnValidate() {
+        if (weaponBase != null)
+        {
+            magnification = weaponBase.magnification;
+            damage = weaponBase.damage;
+            accuracy = weaponBase.accuracy;
+            stability = weaponBase.stability;
+            reloadSpeed = weaponBase.reloadSpeed;
+            fireRate = weaponBase.fireRate;
+            magSize = weaponBase.magSize;
+        }
+
+        if (barrel != null)
+        {
+            accuracy = Mathf.Clamp(accuracy + barrel.accuracyModifier, 0, 1);
+            stability = Mathf.Clamp(stability + barrel.stabilityModifier, 0, 1);
+        }
+
+        if (optics != null)
+        {
+            magnification = optics.magnification;
+        }
+
+        if (magazine != null)
+        {
+            magSize = magazine.magSize;
+            reloadSpeed = magazine.reloadSpeed;
+        }
+    }
+
+
 }
